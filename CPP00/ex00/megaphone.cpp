42182315b9d0e70e6a413deee2ccba8b31697965
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-08-25 19:07:31 +0200

    first CPP program

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   megaphone.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/07/05 14:42:47 by jaberkro      #+#    #+#                 */
+/*   Updated: 2022/08/25 19:04:50 by jaberkro      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+std::string str_toupper(std::string str)
+{
+    for (int i = 0; str[i]; i++)
+    {
+        str[i] = toupper(str[i]);
+    }
+    return str;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return 1;
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        std::cout << str_toupper(argv[i]);
+        if (i < argc - 1)
+            std::cout << ' ';
+    }
+    std::cout << std::endl;
+    return 0;
+}
