6e45253a7087f477ba0634846d88a3ec0a000a70
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-08-26 12:22:12 +0200

    changed destructor message

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/25 19:59:55 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/08/25 20:39:41 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/08/26 12:21:05 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ Zombie::Zombie(std::string name) : _name(name)
 
 Zombie::~Zombie(void)
 {
-    std::cout << "Zombie: Default destructor called on zombie: " << this->_name << std::endl;
+    std::cout << "Zombie: Default destructor called on " << this->_name << std::endl;
 }
 
 void Zombie::announce(void)
