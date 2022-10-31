10bdc075d28fb774ce6543e1e09ee374f14bfe1d
Jaberkro <jaberkro@student.codam.nl>
2022-10-31 16:30:26 +0100

    no spaces printed between variables

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,35 +6,33 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/05 14:42:47 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/08/25 19:27:53 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/10/13 14:27:44 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-std::string str_toupper(std::string str)
+std::string	str_toupper(std::string str)
 {
-    for (int i = 0; str[i]; i++)
-    {
-        str[i] = toupper(str[i]);
-    }
-    return str;
+	for (int i = 0; str[i]; i++)
+	{
+		str[i] = toupper(str[i]);
+	}
+	return str;
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (argc == 1)
-    {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-        return 0;
-    }
-    for (int i = 1; i < argc; i++)
-    {
-        std::cout << str_toupper(argv[i]);
-        if (i < argc - 1)
-            std::cout << ' ';
-    }
-    std::cout << std::endl;
-    return 0;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		std::cout << str_toupper(argv[i]);
+	}
+	std::cout << std::endl;
+	return 0;
 }
