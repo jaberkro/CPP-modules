bc853865a3c434ea8c1020eb28dd5c134cba0f8f
Jaberkro <jaberkro@student.codam.nl>
2022-10-31 17:15:59 +0100

    made print statements more clear

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,12 +6,11 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/26 15:44:56 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/08/26 16:08:29 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/10/31 17:15:25 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cctype>
 
 int	main(void)
 {
@@ -19,13 +18,13 @@ int	main(void)
 	std::string	*stringPTR = &theString;
 	std::string &stringREF = theString;
 
-	std::cout << &theString << std::endl;
-	std::cout << &stringPTR << std::endl;
-	std::cout << &stringREF << std::endl;
+	std::cout << "Address of the string variable:   " << &theString << std::endl;
+	std::cout << "Memory address held by stringPTR: " << &stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << "\n" << std::endl;
 	
-	std::cout << theString << std::endl;
-	std::cout << *stringPTR << std::endl;
-	std::cout << stringREF << std::endl;
+	std::cout << "The value of the string variable:  " << theString << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
 
 	return (0);
 }
