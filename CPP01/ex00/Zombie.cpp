ac5110d61dcb7c7fbaa77e16d639315d9cb26c30
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-01-11 14:58:21 +0100

    removed spaces in function prototype

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -6,29 +6,29 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/25 19:59:55 by jaberkro      #+#    #+#                 */
-/*   Updated: 2023/01/05 16:22:31 by jaberkro      ########   odam.nl         */
+/*   Updated: 2023/01/11 14:57:38 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Zombie.hpp"
 
-Zombie::Zombie( void ) : _name("Jacob")
+Zombie::Zombie(void) : _name("Jacob")
 {
     std::cout << "Zombie: Default constructor called on " << this->_name << std::endl;
 }
 
-Zombie::Zombie( std::string name ) : _name(name)
+Zombie::Zombie(std::string name) : _name(name)
 {
     std::cout << "Zombie: Default constructor called on " << this->_name << std::endl;
 }
 
-Zombie::~Zombie( void )
+Zombie::~Zombie(void)
 {
     std::cout << "Zombie: Default destructor called on " << this->_name << std::endl;
 }
 
-void Zombie::announce( void )
+void Zombie::announce(void)
 {
     std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- CPP01/ex00/Zombie.hpp
+++ CPP01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/25 19:59:58 by jaberkro      #+#    #+#                 */
-/*   Updated: 2023/01/11 13:17:23 by jaberkro      ########   odam.nl         */
+/*   Updated: 2023/01/11 14:57:12 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ class Zombie
 {
     public:
         Zombie();
-        Zombie( std::string name );
+        Zombie(std::string name);
         ~Zombie();
         
-        void announce( void );
+        void announce(void);
 
     private:
         std::string  _name;
 };
 
-Zombie* newZombie( std::string name );
-void randomChump( std::string name );
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
 
 #endif
