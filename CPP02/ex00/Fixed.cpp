49551e664bdc3e3b68275b57b0d34d71f3909bba
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-01-18 16:39:28 +0100

    initialised list or copy assignmend operator. both possible

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/18 12:20:00 by jaberkro      #+#    #+#                 */
-/*   Updated: 2023/01/18 16:04:00 by jaberkro      ########   odam.nl         */
+/*   Updated: 2023/01/18 16:38:03 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ Fixed::Fixed(void) : _value(0)
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &fixed)
+Fixed::Fixed(const Fixed &fixed) //: _value(fixed._value)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = fixed;
