2c2a2a91855dcb516fd734b3cbeb8cef6e9d43d7
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-02-13 17:34:31 +0100

    added tests

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -6,35 +6,58 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/09 19:05:31 by jaberkro      #+#    #+#                 */
-/*   Updated: 2023/02/13 16:33:02 by jaberkro      ########   odam.nl         */
+/*   Updated: 2023/02/13 17:28:13 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include <iostream>
 
 int	main(void)
 {
-	ClapTrap harl("Harl");
-	ClapTrap helga("Helga");
-	
-	harl.attack("Helga");
-	harl.attack("Helga");
-	harl.attack("Helga");
-	harl.attack("Helga");
-	harl.attack("Helga");
-	harl.attack("Helga");
-	harl.attack("Helga");
-	harl.attack("Helga");
-	harl.attack("Helga");
-	harl.attack("Helga");
-	
-	harl.attack("Helga");
-	harl.takeDamage(12);
-	harl.beRepaired(3);
-	helga.attack("Harl");
-	helga.beRepaired(10);
-	helga.takeDamage(10);
-	helga.takeDamage(10);
-	helga.beRepaired(10);
+	{
+		ClapTrap nameless;
+
+		nameless.attack("Peter");
+		nameless.takeDamage(1);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.beRepaired(3);
+		nameless.takeDamage(3);
+		nameless.takeDamage(40);
+	}
+	std::cout << std::endl;
+	{
+		ClapTrap harl("Harl");
+		ClapTrap helga = harl;
+		
+		harl.attack("Helga");
+		harl.attack("Helga");
+		harl.attack("Helga");
+		harl.attack("Helga");
+		harl.attack("Helga");
+		harl.attack("Helga");
+		harl.attack("Helga");
+		harl.attack("Helga");
+		harl.attack("Helga");
+		harl.attack("Helga");
+		
+		harl.attack("Helga");
+		harl.takeDamage(12);
+		harl.beRepaired(3);
+		helga.attack("Harl");
+		helga.beRepaired(10);
+		helga.takeDamage(10);
+		helga.takeDamage(10);
+		helga.beRepaired(10);
+	}
 	return (0);
 }
