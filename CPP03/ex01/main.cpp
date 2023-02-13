e7c315ef6d385adfea009cd7d3acf2376f3bcdf9
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-02-13 17:35:44 +0100

    tests added

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/09 19:05:38 by jaberkro      #+#    #+#                 */
-/*   Updated: 2023/02/13 16:52:14 by jaberkro      ########   odam.nl         */
+/*   Updated: 2023/02/13 17:32:26 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,19 @@
 
 int	main(void)
 {
+	ScavTrap nameless;
 	ScavTrap sally("Sally");
-	
+	ScavTrap copyCat = nameless;
+
+	nameless.guardGate();
 	sally.guardGate();
-	sally.attack("Henry");
+	copyCat.guardGate();
+
+	sally.attack("copyCat");
+	copyCat.takeDamage(200);
 	sally.beRepaired(10);
+	copyCat.beRepaired(10);
+	nameless.attack("Sally");
 	sally.takeDamage(110);
 	sally.beRepaired(10);
 	sally.guardGate();
