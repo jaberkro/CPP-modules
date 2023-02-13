2fba48cc73fdd1620d1f2a633ccf425db7108734
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-02-13 16:19:42 +0100

    copy constructor named correctly

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -6,21 +6,21 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/25 19:59:55 by jaberkro      #+#    #+#                 */
-/*   Updated: 2023/01/11 14:57:38 by jaberkro      ########   odam.nl         */
+/*   Updated: 2023/02/13 16:19:09 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Zombie.hpp"
 
-Zombie::Zombie(void) : _name("Jacob")
+Zombie::Zombie(void): _name("Jacob")
 {
     std::cout << "Zombie: Default constructor called on " << this->_name << std::endl;
 }
 
-Zombie::Zombie(std::string name) : _name(name)
+Zombie::Zombie(std::string name): _name(name)
 {
-    std::cout << "Zombie: Default constructor called on " << this->_name << std::endl;
+    std::cout << "Zombie: Copy constructor called on " << this->_name << std::endl;
 }
 
 Zombie::~Zombie(void)
