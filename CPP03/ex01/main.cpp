f33b50b0b97cb243583a328ed711a24e7f3926e0
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-02-23 10:57:36 +0100

    added newlines and one more scav

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -6,23 +6,28 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/09 19:05:38 by jaberkro      #+#    #+#                 */
-/*   Updated: 2023/02/13 17:32:26 by jaberkro      ########   odam.nl         */
+/*   Updated: 2023/02/23 10:53:36 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 #include "ClapTrap.hpp"
+#include <iostream>
 
 int	main(void)
 {
 	ScavTrap nameless;
 	ScavTrap sally("Sally");
 	ScavTrap copyCat = nameless;
-
+	ScavTrap *newScav = new ScavTrap(sally);
+	
+	std::cout << std::endl;
 	nameless.guardGate();
 	sally.guardGate();
 	copyCat.guardGate();
-
+	newScav->guardGate();
+	
+	std::cout << std::endl;
 	sally.attack("copyCat");
 	copyCat.takeDamage(200);
 	sally.beRepaired(10);
@@ -32,5 +37,9 @@ int	main(void)
 	sally.beRepaired(10);
 	sally.guardGate();
 	sally.attack("Henry");
+	
+	std::cout << std::endl;
+	delete newScav;
+	std::cout << std::endl;
 	return (0);
 }
