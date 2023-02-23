84a979fef4dd10839f1de61027e40507a44e855b
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-02-23 11:01:44 +0100

    newlines added and one more request for highFivesGuys

--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -6,20 +6,24 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/09 19:05:46 by jaberkro      #+#    #+#                 */
-/*   Updated: 2023/02/13 17:46:01 by jaberkro      ########   odam.nl         */
+/*   Updated: 2023/02/23 10:59:53 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
+#include <iostream>
 
 int	main(void)
 {
 	FragTrap frog("Frog");
 
+	std::cout << std::endl;
+	frog.highFivesGuys();
 	frog.attack("Chicken");
 	frog.beRepaired(100);
 	frog.takeDamage(1000);
 	frog.takeDamage(10);
 	frog.highFivesGuys();
+	std::cout << std::endl;
 	return (0);
 }
