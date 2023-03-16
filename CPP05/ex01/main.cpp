3c0436f583bad1b1f866c5d696eca62d3fa091d7
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-03-16 16:33:11 +0100

    added constructor test

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -3,6 +3,19 @@
 
 int	main(void)
 {
+
+	std::cout << "\n-----------------------------CONSTRUCTION FAILS-----------------------------" << std::endl;
+	try
+	{
+		Form fail("Fail", 250, 100);
+		std::cout << fail << std::endl;
+
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
 	Form a("A", 50, 100);
 	Form b("B", 21, 120);
 	Form c("C", 2, 150);
@@ -10,7 +23,7 @@ int	main(void)
 
 	Bureaucrat harl("Harl", 20);
 
-	std::cout << std::endl;
+	std::cout << "\n-----------------------------WORKS-----------------------------" << std::endl;
 	try
 	{
 		std::cout << a << std::endl;
@@ -22,7 +35,7 @@ int	main(void)
 		std::cerr << e.what() << '\n';
 	}
 
-	std::cout << std::endl;
+	std::cout << "\n-----------------------------WORKS-----------------------------" << std::endl;
 	try
 	{
 		std::cout << b << std::endl;
@@ -34,17 +47,19 @@ int	main(void)
 		std::cerr << e.what() << '\n';
 	}
 
+	std::cout << "\n-----------------------------GRADE TOO LOW-----------------------------" << std::endl;
 	std::cout << std::endl;
 	std::cout << c << std::endl;
 	harl.signForm(c);
 	std::cout << c << std::endl;
 
-	std::cout << std::endl;
+	std::cout << "\n-----------------------------WORKS-----------------------------" << std::endl;
 	std::cout << d << std::endl;
 	harl.signForm(d);
 	std::cout << d << std::endl;
 	std::cout << std::endl;
-
+	
+	std::cout << "\n-----------------------------WORKS (WITH NEW)-----------------------------" << std::endl;
 	Form *f = NULL;
 
 	try
