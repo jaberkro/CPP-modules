5d54813a8eb2c1f9c4c6bb98dc1112bb48ccbac0
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-03-30 19:28:29 +0200

    identity functions working and tested.

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -6,39 +6,83 @@
 
 Base * generate(void)
 {
-	Base *basePointer;
+	Base *basePointer = NULL;
 
 	switch(std::rand() % 3)
 	{
 		case 0:
 			basePointer = dynamic_cast<Base *>(new A);
+			break ;
 		case 1:
 			basePointer = dynamic_cast<Base *>(new B);
+			break ;
 		case 2:
 			basePointer = dynamic_cast<Base *>(new C);
+			break ;
 	}
 	return (basePointer);
 }
 
-// void identify(Base* p)
-// {
-// 	(void)p;
+void identify(Base* p)
+{
+	if (dynamic_cast<A *>(p) == p)
+		std::cout << "A" << std::endl;
+	if (dynamic_cast<B *>(p) == p)
+		std::cout << "B" << std::endl;
+	if (dynamic_cast<C *>(p) == p)
+		std::cout << "C" << std::endl;
+}
 
-// }
-
-// void identify(Base& p)
-// {
-// 	(void)p;
-// }
-
+void identify(Base& p)
+{
+	try 
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch(std::bad_cast)
+	{
+		try
+		{
+			(void)dynamic_cast<B&>(p);
+			std::cout << "B" << std::endl;
+		}
+		catch(std::bad_cast)
+		{
+			try
+			{
+				(void)dynamic_cast<C&>(p);
+				std::cout << "C" << std::endl;
+			}
+			catch(std::bad_cast)
+			{
+				std::cout << "Error: type does not respond to A, B or C" << std::endl;
+			}	
+		}
+	}
+}
 
 int	main(void)
 {
 	std::srand(std::time(nullptr));
 
-	Base	*ptr;
+	Base	*random1;
+	Base	*random2;
+	Base	*random3;
+	Base	*random4;
 
-	ptr = generate();
+	random1 = generate();
+	random2 = generate();
+	random3 = generate();
+	random4 = generate();
+	identify(random1);
+	identify(*random1);
+	identify(random2);
+	identify(*random2);
+	identify(random3);
+	identify(*random3);
+	identify(random4);
+	identify(*random4);
 
 	return (0);
 }
